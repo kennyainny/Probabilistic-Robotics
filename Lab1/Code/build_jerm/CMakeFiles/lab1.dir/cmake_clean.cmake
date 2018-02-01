b3164194c2a37f4df6dda2ee30ff09fed0a40ff0
jepps6 <jeremy.epps@gatech.edu>
2018-02-01 15:41:18 -0500

    jeremy

---
+++ Lab1/Code/build_jerm/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/src/bee-map.c.o"
+  "../bin/lab1.pdb"
+  "../bin/lab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
