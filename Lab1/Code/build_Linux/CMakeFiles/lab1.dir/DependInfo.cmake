4b76a94ebac2b38f39ab936fcc6220b9c2363357
numkang <thanakornkhamvilai@gmail.com>
2018-02-01 14:09:28 -0500

    test

---
+++ Lab1/Code/build_Linux/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/thanakorn/OneDrive/Graduate/Spring2018/Statistical Techniques in Robotics/Lab/Lab1/MyCode/src/HelloWorld.c" "/home/thanakorn/OneDrive/Graduate/Spring2018/Statistical Techniques in Robotics/Lab/Lab1/MyCode/build_Linux/CMakeFiles/lab1.dir/src/HelloWorld.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab1/Code/build_Windows/CMakeFiles/hell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hell.dir/HelloWorld.cpp.obj"
+  "hell.pdb"
+  "hell.exe"
+  "hell.exe.manifest"
+  "libhell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
