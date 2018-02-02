8c762a1bcde70ed8a070eb3c565d116f175d9820
numkang <thanakornkhamvilai@gmail.com>
2018-02-01 21:00:49 -0500

    a

--- Lab1/Code/build_Linux/CMakeFiles/lab1.dir/cmake_clean.cmake
+++ Lab1/Code/build_Linux/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -1,19 +1,11 @@
 file(REMOVE_RECURSE
-<<<<<<< HEAD
-  "CMakeFiles/lab1.dir/src/main.c.o"
-=======
   "CMakeFiles/lab1.dir/src/main.cpp.o"
->>>>>>> 769531312ee81a4a3a28d39ea559a7c713e35b4a
   "CMakeFiles/lab1.dir/src/map.c.o"
-  "../bin/lab1.pdb"
-  "../bin/lab1"
+  "../bin/======lab1.pdb"
+  "../bin/======lab1"
 )
 
 # Per-language clean rules from dependency scanning.
-<<<<<<< HEAD
-foreach(lang C)
-=======
 foreach(lang C CXX)
->>>>>>> 769531312ee81a4a3a28d39ea559a7c713e35b4a
   include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
